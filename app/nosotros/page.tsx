2986ff8997c0a41dd1dd3c6858a9792ed0515fd4
artmadeit <this.arthur.email@gmail.com>
2023-05-24 22:13:05 -0500

    feat: iteracion for de veterinarios

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -1,3 +1,16 @@
+const veterinarios = [
+  {
+    id: 1,
+    nombre: "Alex",
+    especialidad: "Oncologia",
+  },
+  {
+    id: 2,
+    nombre: "Malena",
+    especialidad: "Oncologia",
+  },
+];
+
 export default function Nosotros() {
   return (
     <div>
@@ -5,6 +18,36 @@ export default function Nosotros() {
       <p>Somos una empresa dedicada ...</p>
       <h3>Objetivo</h3>
       <p>Nuestra meta en el 2026 es lograr a ser</p>
+
+      {veterinarios.map((veterinario) => (
+        <VeterinarioCard
+          key={veterinario.id}
+          nombre={veterinario.nombre}
+          especialidad={veterinario.especialidad}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface Veterinario {
+  nombre: string;
+  especialidad: string;
+}
+
+function VeterinarioCard({ nombre, especialidad }: Veterinario) {
+  return (
+    <div style={{ border: "2px solid black" }}>
+      <h4>{nombre}</h4>
+      <p>
+        <strong>{especialidad}</strong>
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore hic
+        explicabo nisi modi sint, eligendi iure ratione quas voluptate
+        consectetur laboriosam autem ipsam. Dolor, deleniti ipsa. Aliquam
+        suscipit hic voluptatem.
+      </p>
     </div>
   );
 }
