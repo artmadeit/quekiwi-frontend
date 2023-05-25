abe7261e541ffb84098a2350be7be295446a07ed
artmadeit <this.arthur.email@gmail.com>
2023-05-24 21:40:11 -0500

    feat: nuevas paginas nosotros y contactenos

---
+++ app/contactenos/page.tsx
@@ -0,0 +1,9 @@
+export default function Contactenos() {
+  return (
+    <div>
+      <h1>Contactenos</h1>
+      <p>Direccion: San borja</p>
+      <p>Telefono: +51 2903902</p>
+    </div>
+  );
+}
---
+++ app/nosotros/page.tsx
@@ -0,0 +1,10 @@
+export default function Nosotros() {
+  return (
+    <div>
+      <h1>Nosotros</h1>
+      <p>Somos una empresa dedicada ...</p>
+      <h3>Objetivo</h3>
+      <p>Nuestra meta en el 2026 es lograr a ser</p>
+    </div>
+  );
+}
