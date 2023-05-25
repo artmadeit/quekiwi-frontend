b511611c9fa49da6eb04dcda9996906bd0b001f8
artmadeit <this.arthur.email@gmail.com>
2023-05-24 21:33:52 -0500

    feat: 1ra pagina home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
 export default function Home() {
-  return <main>Hola Mundo</main>;
+  return (
+    <main>
+      <Header />
+      <h1>Quekiwi</h1>
+      <p>
+        Somos una veterinaria que cuida a tus mascotas como si fueran nuestras
+      </p>
+      <Footer />
+    </main>
+  );
+}
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return <footer>Copyright © QueKiwi {year}</footer>;
+}
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Inicio</li>
+          <li>Nosotros</li>
+          <li>Contactenos</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
